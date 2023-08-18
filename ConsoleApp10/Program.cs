16ff58cfd6d8c36c0209154725e4da1bb3ba946b
Aleksandra Olejarz <oluta525@gmail.com>
2023-08-18 03:41:51 +0200

    added class Rose, derived from the abstract class Plant, with overriding of the abstract methods

--- ConsoleApp10/Program.cs
+++ ConsoleApp10/Program.cs
@@ -1,1 +1,41 @@
 ﻿//abstract classes and methods
+ 
+abstract class Plant
+{
+    public abstract void planting();
+    public abstract void watering();
+    public void selling ()
+    {
+        double price1 = Convert.ToDouble(Console.ReadLine());
+        double price2 = Convert.ToDouble(Console.ReadLine());
+        double total = price1 + price2;
+        Console.WriteLine("The total equals to: "+total);
+    }
+}
+
+class Rose : Plant
+{
+    public string colour;
+    public double weigth;
+
+    public Rose(string colour, double weigth)
+    {
+        this.colour = colour;
+        this.weigth = weigth;
+    }
+
+    public override void planting()
+
+    {
+        Console.WriteLine("How many roses have you planted today? ");
+        int roses = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Today, you've planted "+ roses + " roses");
+
+    }
+    public override void watering()
+    {
+        Console.WriteLine("Watering is very important for plants, your roses will grow healthy if you water them well");
+    }
+
+}
+
